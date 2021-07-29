5b469ad80677d48dfa83dd3d72798fed34484f6e
Ostask <502724802@qq.comy>
2021-07-29 15:05:20 +0800

    封装指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -2,8 +2,15 @@ import Accelerator from "../accelerator"
 
 const directive = {
     // 当被绑定的元素插入到 DOM 中时……
-    bind: function (el,binding) {
-       el.ackun = new Accelerator(el,binding)
+    inserted: function (el,binding) {
+       el.ackun = new Accelerator(el,binding.value)
+    },
+    update: function(el,binding) {
+        el.ackun.destroy()
+        el.ackun = new Accelerator(el,binding.value)
+    },
+    unbind: function(el) {
+        el.ackun.destroy()
     }
 }
 
